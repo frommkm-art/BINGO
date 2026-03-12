b209d66a5757242f1e0a9b5ea2bead146d7a6252
Your Name <your@email.com>
2026-03-12 15:35:51 +0530

    fix double server listen

--- backend/server.js
+++ backend/server.js
@@ -176,6 +176,8 @@ socket.join(room)
 
 })
 
-server.listen(7000,()=>{
-console.log("Bingo Pro running on port 7000")
+const PORT = process.env.PORT || 3000
+
+server.listen(PORT, () => {
+  console.log("Server running on port " + PORT)
 })
