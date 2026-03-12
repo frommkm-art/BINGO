b6ea27201e65f411f25a477c7fbd746306404f60
Your Name <your@email.com>
2026-03-12 14:02:23 +0530

    server frontend

--- server.js
+++ server.js
@@ -1,11 +1,15 @@
-const express = require("express");
-const http = require("http");
-const { Server } = require("socket.io");
-const cors = require("cors");
+const express = require("express")
+const http = require("http")
+const { Server } = require("socket.io")
+const path = require("path")
 
-const app = express();
-app.use(cors());
-app.use(express.json());
+const app = express()
+const server = http.createServer(app)
+const io = new Server(server)
+
+app.use(express.json())
+
+app.use(express.static(path.join(__dirname,"frontend")))
 
 const PORT = process.env.PORT || 7000;
 
