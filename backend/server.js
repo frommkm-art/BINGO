33a906eb66adf2e042de8a089812ffca69e313d3
Your Name <your@email.com>
2026-03-12 14:27:54 +0530

    fix static frontend

--- backend/server.js
+++ backend/server.js
@@ -9,13 +9,14 @@ const io = new Server(server);
 
 app.use(express.json());
 
-app.use(express.static(path.join(__dirname, "../frontend")));
+app.use(express.static(path.join(__dirname, "../public")));
 
 app.get("/", (req, res) => {
-  res.sendFile(path.join(__dirname, "../frontend", "index.html"));
+  res.sendFile(path.join(__dirname, "../public", "index.html"));
 });
 
 const PORT = process.env.PORT || 7000;
+
 server.listen(PORT, () => {
   console.log("Server running on port", PORT);
 });
